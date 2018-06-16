52127b7e2200f83ff61b4a5aa66028ac95d6fd02
Prashanth Moorthy <chapparfellow@gmail.com>
2018-06-16 08:06:21 +0530

    Initial Push to GitHub

---
+++ AchiTest.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import GameKit
+
+var str = "Hello, playground"
+
+class GameBoardPosition {
+    
+}
+
+class GameBoard : GKGameModel {
+    
+    func isValidMove(from : Gam)
+    
+}
+
+class GamePlayer : GKGameModelPlayer {
+    func makeMove()
+}
+
+struct GameMove : GKGameModelUpdate{
+    
+}
+
